4d24dcd1054d455a9975567d34d957e562359666
js14094 <js14094@nyu.edu>
2023-10-10 20:00:39 -0400

    add dns entry safebank

--- DNSServer.py
+++ DNSServer.py
@@ -83,6 +83,13 @@ dns_records = {
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.NS: 'ns1.nyu.edu.',
         dns.rdatatype.TXT: (encrypted_value.decode('utf-8'),),
+    },
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
+        dns.rdatatype.TXT: (encrypted_value.decode('utf-8'),),
     }
    
     # Add more records as needed (see assignment instructions!
